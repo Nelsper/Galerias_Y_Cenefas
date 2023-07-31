eb96cfbea578ca01808c8357c2f6bffc17662753
Nelsper <nelsper@gmail.com>
2023-07-31 18:25:47 -0500

    componente Promo o.k

--- src/components/promo/Promo.jsx
+++ src/components/promo/Promo.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import './sass/promo.css'
+import "./sass/promo.css";
 
 const Promo = () => {
-
-    return(
-        <>
-            <div className="promo">
-                <marquee behavior="" direction="raigth">Hola Mundo</marquee>
-            </div>
-        </>
-    );
+  return (
+    <>
+    <div className="contenedor">
+      <div className="promo">
+        
+        <span className="promo__text">
+          Galerias Y Cenefas&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+          Calle 41b sur # 52a 56&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Barrio Alqueria&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Tel. 310 787 8993
+          </span>
+           
+      </div>
+    </div>
+    </>
+  );
 };
 
 export default Promo;
